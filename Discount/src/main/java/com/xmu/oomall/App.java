b58406af4caddfe18f541cb75693ed0fb1a36e5f
neilhanhan <1138418834@qq.com>
2019-12-08 12:10:13 +0800

    Initial commit

---
+++ Discount/src/main/java/com/xmu/oomall/App.java
@@ -0,0 +1,13 @@
+package com.xmu.oomall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Discount/src/main/java/com/xmu/oomall/controller/CouponController.java
@@ -0,0 +1,10 @@
+package com.xmu.oomall.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Liuwenhan
+ */
+@RestController
+public class CouponController {
+}
